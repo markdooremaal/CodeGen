56f16e65a85c96a5281f599b4879a497ac947203
Mark van Dooremaal <vandooremaal@gmail.com>
2021-05-31 11:21:54 +0200

    Get bankaccount based on iban

--- src/main/java/io/swagger/repository/IBankAccountRepository.java
+++ src/main/java/io/swagger/repository/IBankAccountRepository.java
@@ -4,6 +4,8 @@ import io.swagger.model.ArrayOfBankAccounts;
 import io.swagger.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IBankAccountRepository extends JpaRepository<BankAccount, Integer> {
+public interface IBankAccountRepository extends JpaRepository<BankAccount, String> {
     ArrayOfBankAccounts findAll();
+
+    BankAccount findByIbanLike(String iban);
 }
--- src/main/java/io/swagger/service/BankAccountService.java
+++ src/main/java/io/swagger/service/BankAccountService.java
@@ -1,6 +1,7 @@
 package io.swagger.service;
 
 import io.swagger.model.ArrayOfBankAccounts;
+import io.swagger.model.BankAccount;
 import io.swagger.repository.IBankAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,14 @@ public class BankAccountService {
 
     public ArrayOfBankAccounts getAllBankAccounts() {
         return bankAccountRepository.findAll();
+    }
+
+    public BankAccount getBankAccountByIban(String iban) {
+        return bankAccountRepository.findByIbanLike(iban);
+    }
+
+    public void storeBankAccount(BankAccount bankAccount) {
+        //:TODO Iban willekeurig op basis van template?
+        bankAccountRepository.save(bankAccount);
     }
 }
